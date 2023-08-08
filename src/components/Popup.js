ae412ada2467ad233dea8d633f51c1e2b4fa6992
Alvaro <alvaroibarra11@hotmail.com>
2023-08-08 09:36:43 -0600

    feat: project 10 deploy project configuration

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,7 +5,7 @@ export default class Popup {
 
   open() {
     this._popupElement.classList.add('popup_opened');
-    document.addEventListener("keydown", this._handleEscClose);
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
