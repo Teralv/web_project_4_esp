f7475f15702da390a0a6d949d2c5fdd57648b0a0
Alvaro <alvaroibarra11@hotmail.com>
2024-03-07 10:41:34 -0600

    test corrections

--- src/components/Card.js
+++ src/components/Card.js
@@ -63,9 +63,9 @@ export default class Card {
       this._handleDeleteClick({id: this._id});
     });
 
-    if (this._ownerId !== this._user) {
+    /*if (this._ownerId !== this._user) {
       this._element.querySelector('.element__trash').remove();
-    }
+    }*/
   }
 
   addLikeFilter() {
